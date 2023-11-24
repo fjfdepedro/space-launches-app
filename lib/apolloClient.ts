41a4403bd4331be2049230bda40f231fa56ec0ad
Paco Fernández <fjfdepedro@gmail.com>
2023-11-24 12:42:54 +0100

    Refactor apolloClient.ts to import DocumentNode at the top and remove duplicate import.

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,11 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { DocumentNode } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://spacex-production.up.railway.app/',
   cache: new InMemoryCache()
 });
-
-import { DocumentNode } from '@apollo/client';
 
 export async function fetchData(query: DocumentNode) {
   const { data } = await client.query({
