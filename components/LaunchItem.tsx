8bcf052048cc06ca3b465dd2c096ad39e2cef727
Paco Fernández <fjfdepedro@gmail.com>
2023-11-24 14:36:54 +0100

    aider: Fix the issue with parsing the `launch_date_local` by setting the `timeZone` option to 'UTC' when converting it to a date string in the `LaunchItem` component.
    # Aider chat conversation:
    
    USER: the launch_date_local is bad parsed,  i got of api this "2020-06-03T21:25:00-04:00"
    
    ASSISTANT: I see, the `launch_date_local` is in ISO 8601 format. This format can be directly passed to the JavaScript `Date` constructor to create a `Date` object.
    
    However, in your `LaunchItem` component, you're using `new Date(launch.launch_date_local).toLocaleDateString("en-US")` to convert the `launch_date_local` to a date string. This should work correctly with the ISO 8601 date string you're getting from the API.
    
    If you're seeing incorrect dates, it could be due to a timezone issue. The `toLocaleDateString` method converts the date to a string using the current timezone of the user's system. If the `launch_date_local` is in a different timezone, this could result in the date being off by a day.
    
    To fix this, you can use the `toLocaleDateString` method with the `timeZone` option set to 'UTC'. This will convert the date to a string using Coordinated Universal Time (UTC), which should give you the correct date regardless of the user's system timezone.
    
    Here's the change you need to make:
    
    <source>typescript
    components/LaunchItem.tsx
    <<<<<<< HEAD
          <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US") }</p>
    =======
          <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US", {timeZone: 'UTC'}) }</p>
    >>>>>>> updated
    </source>
    
    This change will ensure that the `launch_date_local` is correctly converted to a date string in the `LaunchItem` component.

--- components/LaunchItem.tsx
+++ components/LaunchItem.tsx
@@ -4,7 +4,7 @@ export function LaunchItem({ launch }: { launch: Launch }) {
   return (
     <div key={launch.id} className="block p-4 m-2 border rounded shadow">
       <h3 className="text-xl font-bold">{ launch.mission_name }</h3>
-      <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US") }</p>
+      <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US", {timeZone: 'UTC'}) }</p>
       <p><strong>Details:</strong> { launch.details ? launch.details : 'N/A' }</p>
       <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
       <p><strong>Article Link:</strong> <a href={launch.links.article_link}>{launch.links.article_link}</a></p>
