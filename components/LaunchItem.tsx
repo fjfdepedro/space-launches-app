57e27b622011cdf6441fa810a942bab8e39f59ea
Paco Fernández <fjfdepedro@gmail.com>
2023-11-24 14:52:09 +0100

    aider: Refactored `components/LaunchItem.tsx` to wrap the content inside a `div` with a class name of "launch-info" to apply a grid layout.
    # Aider chat conversation:
    
    USER: i want to see `components/LaunchItem.tsx` in columns and files
    
    ASSISTANT: To display the information in columns and files, we can use CSS Grid or Flexbox. Here, I'll use CSS Grid for this purpose.
    
    We need to modify the `components/LaunchItem.tsx` file.
    
    Here are the changes:
    
    1. Wrap the content inside the div with a class name of "launch-info" to apply the grid layout.
    2. Add a new CSS class "launch-info" to style the grid layout.
    
    Here is the *edit block*:
    
    ```tsx
    components/LaunchItem.tsx
    <<<<<<< HEAD
        <div key={launch.id} className="block p-4 m-2 border rounded shadow">
          <h3 className="text-xl font-bold">{ launch.mission_name }</h3>
          <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("es-ES", {weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric', timeZone: 'UTC'}) }, { new Date(launch.launch_date_local).toLocaleTimeString("es-ES", {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC'}) }</p>
          <p><strong>Details:</strong> { launch.details ? launch.details : 'N/A' }</p>
          <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
          <p><strong>Article Link:</strong> <a href={launch.links.article_link}>{launch.links.article_link}</a></p>
          <p><strong>Video Link:</strong> <a href={launch.links.video_link}>{launch.links.video_link}</a></p>
          <p><strong>Images:</strong>
          {launch.links.flickr_images.map((image, index) => (
            <img key={index} src={image} alt={`Launch ${launch.mission_name} Image ${index + 1}`} />
          ))}
          </p>
        </div>
    =======
        <div key={launch.id} className="block p-4 m-2 border rounded shadow">
          <div className="launch-info">
            <h3 className="text-xl font-bold">{ launch.mission_name }</h3>
            <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("es-ES", {weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric', timeZone: 'UTC'}) }, { new Date(launch.launch_date_local).toLocaleTimeString("es-ES", {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC'}) }</p>
            <p><strong>Details:</strong> { launch.details ? launch.details : 'N/A' }</p>
            <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
            <p><strong>Article Link:</strong> <a href={launch.links.article_link}>{launch.links.article_link}</a></p>
            <p><strong>Video Link:</strong> <a href={launch.links.video_link}>{launch.links.video_link}</a></p>
            <p><strong>Images:</strong>
            {launch.links.flickr_images.map((image, index) => (
              <img key={index} src={image} alt={`Launch ${launch.mission_name} Image ${index + 1}`} />
            ))}
            </p>
          </div>
        </div>
    >>>>>>> updated
    ```
    
    Please note that you will need to add the CSS for the "launch-info" class in your CSS file. Here is an example of how you can style it:
    
    ```css
    .launch-info {
      display: grid;
      grid-template-columns: repeat(2, 1fr);
      gap: 20px;
    }
    ```
    
    Please add your CSS file to the chat so I can propose the changes there.

--- components/LaunchItem.tsx
+++ components/LaunchItem.tsx
@@ -3,17 +3,19 @@ import type { Launch } from '../types';
 export function LaunchItem({ launch }: { launch: Launch }) {
   return (
     <div key={launch.id} className="block p-4 m-2 border rounded shadow">
-      <h3 className="text-xl font-bold">{ launch.mission_name }</h3>
-      <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("es-ES", {weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric', timeZone: 'UTC'}) }, { new Date(launch.launch_date_local).toLocaleTimeString("es-ES", {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC'}) }</p>
-      <p><strong>Details:</strong> { launch.details ? launch.details : 'N/A' }</p>
-      <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
-      <p><strong>Article Link:</strong> <a href={launch.links.article_link}>{launch.links.article_link}</a></p>
-      <p><strong>Video Link:</strong> <a href={launch.links.video_link}>{launch.links.video_link}</a></p>
-      <p><strong>Images:</strong>
-      {launch.links.flickr_images.map((image, index) => (
-        <img key={index} src={image} alt={`Launch ${launch.mission_name} Image ${index + 1}`} />
-      ))}
-      </p>
+      <div className="launch-info">
+        <h3 className="text-xl font-bold">{ launch.mission_name }</h3>
+        <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("es-ES", {weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric', timeZone: 'UTC'}) }, { new Date(launch.launch_date_local).toLocaleTimeString("es-ES", {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC'}) }</p>
+        <p><strong>Details:</strong> { launch.details ? launch.details : 'N/A' }</p>
+        <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
+        <p><strong>Article Link:</strong> <a href={launch.links.article_link}>{launch.links.article_link}</a></p>
+        <p><strong>Video Link:</strong> <a href={launch.links.video_link}>{launch.links.video_link}</a></p>
+        <p><strong>Images:</strong>
+        {launch.links.flickr_images.map((image, index) => (
+          <img key={index} src={image} alt={`Launch ${launch.mission_name} Image ${index + 1}`} />
+        ))}
+        </p>
+      </div>
     </div>
   );
 }
